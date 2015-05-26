09871d7a374dd8be4b2be6138ef54d74d934f17c
MarcLindorfer <lindorfer.marc@gmail.com>
2015-05-26 21:31:33 +0200

    asdf

---
+++ Bomberman/Bomberman/Model/GameElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleMapLoaderLib;
+
+namespace Bomberman.Model
+{
+    class GameElement : IConsoleMapElement
+    {
+        public ConsoleColor BackGroundColor { get; set; }
+
+        public ConsoleColor ForeGroundColor { get; set; }
+
+        public char ReadSign { get; set; }
+        public char Sign { get; set; }
+
+        public GameElement(ConsoleColor backGroundColor, ConsoleColor foreGroundColor, char readSign, char sign)
+        {
+            this.BackGroundColor = backGroundColor;
+            this.ForeGroundColor = foreGroundColor;
+            this.ReadSign = readSign;
+            this.Sign = sign;
+        }
+    }
+}
