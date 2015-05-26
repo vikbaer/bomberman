deb982e9bd09d194373be6697fe72a367182c320
MarcLindorfer <lindorfer.marc@gmail.com>
2015-05-26 22:01:34 +0200

    Added GameModel

---
+++ Bomberman/Bomberman/Model/GameModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleMapLoaderLib;
+
+namespace Bomberman.Model
+{
+    public delegate void ModelChangedHandler();
+    class GameModel
+    {
+        private List<IConsoleMapElement> consoleMapElements;
+        private ConsoleMapLoader mapLoader;
+
+        public GameModel()
+        {
+            this.consoleMapElements = new List<IConsoleMapElement>();
+            this.consoleMapElements.Add(new Wall(ConsoleColor.Black, ConsoleColor.DarkRed, '#', '#', false));
+            this.consoleMapElements.Add(new Wall(ConsoleColor.Black, ConsoleColor.Red, '+', '#', true));
+            this.consoleMapElements.Add(new None(ConsoleColor.Black, ConsoleColor.Black, ' ', ' '));
+
+            this.mapLoader = new ConsoleMapLoader("Maps", "cmap", this.consoleMapElements);
+        }
+    }
+}
